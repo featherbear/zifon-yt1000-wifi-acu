c2cfa95b49cdfff25c09e2248575397a0bb7a7c7
Andrew Wong <andrew.j.wong@outlook.com>
2021-08-23 21:49:46 +1000

    Mode config

--- acu/http/src/App.svelte
+++ acu/http/src/App.svelte
@@ -1,11 +1,41 @@
-<script>
+<script lang="ts">
   import WiFi from "./WiFi.svelte";
   import PT from "./PT.svelte";
+
+  import { onMount } from "svelte";
+  import { fade } from "svelte/transition";
+
+  let component;
+  onMount(async () => {
+    fetch("/mode")
+      .then((r) => r.text())
+      .then(
+        (mode) =>
+          (component = (function () {
+            switch (mode) {
+              case "config":
+                return WiFi;
+              default:
+                return PT;
+            }
+          })())
+      )
+      .catch(() => {
+        console.error("Could not fetch mode setting, reloading");
+        setTimeout(() => {
+          location.reload();
+        }, 2000);
+      });
+  });
 </script>
 
-<main>
-  <svelte:component this={PT} />
-</main>
+{#if component}
+  <main in:fade>
+    <svelte:component this={component} />
+  </main>
+{:else}
+  <h2>Please wait...</h2>
+{/if}
 
 <style global lang="scss">
   @import "bulma/bulma.sass";
